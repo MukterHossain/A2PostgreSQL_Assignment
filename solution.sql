1c2066e540888b80fcdb8e0220689a05bb988741
MukterHossain <mukter3075@gmail.com>
2025-05-24 22:11:03 +0600

    insert data in rangers table

--- solution.sql
+++ solution.sql
@@ -1,14 +1,14 @@
 
 
-
+-- create rangers table
 CREATE Table rangers(
     ranger_id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     region VARCHAR(100)
 );
 
--- create species table
 
+-- create species table
 CREATE Table species(
     species_id SERIAL PRIMARY KEY,
     common_name VARCHAR(50),
@@ -16,3 +16,36 @@ CREATE Table species(
     discovery_date DATE,
     conservation_status VARCHAR(50)
 );
+
+
+-- create sightings table
+CREATE Table sightings(
+    sighting_id SERIAL PRIMARY KEY,
+    ranger_id INT REFERENCES rangers(ranger_id),
+    species_id INT REFERENCES species(species_id),
+    sighing_time DATE,
+    location VARCHAR(50),
+    notes VARCHAR(200)
+);
+
+
+-- Insert data in rangers table
+INSERT INTO rangers (name, region) VALUES
+('Alice Green', 'Northern Hills'),
+('Bob White', 'River Delta'),
+('Carol King', 'Mountain Range'),
+('Akib Triko', 'Southern Hills'),
+('Kris Oks', 'Soloman Island'),
+('Nippo Shepos', 'Safari Park'),
+('Philip Slikok', 'Pablakhali'),
+('Mamun Hider', 'Subdarbans'),
+('Tariq Bilal', 'Rema Kalenga');
+
+
+-- Insert data in species table
+
+
+-- Insert data in sightings table
+
+
+SELECT * FROM rangers;
