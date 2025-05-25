67267847a0ffa3e433e7e586fa48bcb367e70ea4
MukterHossain <mukter3075@gmail.com>
2025-05-25 09:52:08 +0600

    add  solution of three problem

--- solution.sql
+++ solution.sql
@@ -37,15 +37,62 @@ INSERT INTO rangers (name, region) VALUES
 ('Akib Triko', 'Southern Hills'),
 ('Kris Oks', 'Soloman Island'),
 ('Nippo Shepos', 'Safari Park'),
-('Philip Slikok', 'Pablakhali'),
-('Mamun Hider', 'Subdarbans'),
 ('Tariq Bilal', 'Rema Kalenga');
 
 
 -- Insert data in species table
+INSERT INTO species (common_name, scientific_name, discovery_date, conservation_status ) VALUES
+('Snow Leopard', 'Pnthera uncia', '1775-01-01', 'Endangered'),
+('Red Panda', 'Ailurus fulgens', '1825-01-20', 'Vulrerable'),
+('Bengal Tiger', 'Panthera tigris tigris', '1758-05-01', 'Vulrerable'),
+('Asiatic Elephant', 'Elephas maximus indicus', '1758-01-20', 'Endangered'),
+('African Elephant', 'Loxodonta africana', '1797-03-15', 'Vulrerable'),
+('Tasmanian Devil', 'Sarcophilus harrisii', '1808-01-10', 'Vulrerable'),
+('Vaquita', 'Phocoena sinus', '1958-11-01', 'Endangered'),
+('Snowy Owl', 'Bubo scandiacus', '1758-07-10', 'Vulrerable'),
+('Green Sea Turtle', 'Chelonia mydas', '1802-09-01', 'Vulrerable');
 
 
 -- Insert data in sightings table
+INSERT INTO sightings(species_id, ranger_id,location, sighing_time, notes) VALUES
+(1, 1, 'Peak Ridge', '2024-05-10 07:45:00', 'Camera trap image captured'),
+(2, 2, 'Bankwood Area', '2024-05-15 09:10:00', 'Juvenile seen'),
+(3, 3, 'Bamboo Grove East', '2024-05-12 16:20:00', 'Feeding observed'),
+(1, 2, 'Snowfall Pass', '2024-05-18 18:30:00', 'NULL'),
+(3, 1, 'South Valley', '2024-05-20 10:20:00', 'Spotted with binoculars'),
+(4, 4, 'North Cliffs', '2024-05-09 08:55:00', 'Heard distant calls'),
+(3, 5, 'Eagle Pass', '2024-05-18 18:30:00', 'Juvenile seen'),
+(2, 3, 'Willow Marsh', '2024-05-13 07:05:00', 'Fresh scat found'),
+(1, 7, 'Cedar Trail Pass', '2024-05-18 18:30:00', 'Brief visual comfirmation');
 
+
+
+
+--  Problem 1
+INSERT INTO rangers (name, region) VALUES('Derek Fox', 'Coastal Plains');
+
+
+--  Problem 2
+SELECT count(DISTINCT species_id) FROM sightings;
+
+
+
+
+
+--  Problem 3
+SELECT * FROM sightings WHERE location LIKE '%Pass';
+
+
+
+
+--  Problem 4
 
 SELECT * FROM rangers;
+SELECT * FROM species;
+SELECT * FROM sightings;
+
+
+
+DROP TABLE rangers;
+DROP TABLE species;
+DROP TABLE sightings;
